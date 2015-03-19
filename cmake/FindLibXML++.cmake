db2bdc64abc324a067a3918bf7d5982af505f417
Anthony Scopatz <scopatz@gmail.com>
2015-03-19 10:55:33 -0500

    libxml++ fixes

--- cmake/FindLibXML++.cmake
+++ cmake/FindLibXML++.cmake
@@ -11,12 +11,22 @@ include(LibFindMacros)
 libfind_package(LibXML++ LibXml2)
 libfind_package(LibXML++ Glibmm)
 
+#
+# Get the root directory hint if provided
+#
+IF(NOT DEFINED LIBXMLXX_ROOT_DIR)
+    SET(LIBXMLXX_ROOT_DIR "$ENV{LIBXMLXX_ROOT_DIR}")
+    MESSAGE("\tLibXML++ Root Dir: ${LIBXMLXX_INCLUDE_DIR}")
+ENDIF(NOT DEFINED LIBXMLXX_ROOT_DIR)
+MESSAGE(STATUS "LIBXMLXX_ROOT_DIR hint is : ${LIBXMLXX_ROOT_DIR}")
+
 # Use pkg-config to get hints about paths
 libfind_pkg_check_modules(LibXML++_PKGCONF libxml++-2.6)
 
 # Main include dir
 find_path(LibXML++_INCLUDE_DIR
     NAMES libxml++/libxml++.h
+    HINTS "${LIBXMLXX_ROOT_DIR}/include"
     PATHS ${LibXML++_PKGCONF_INCLUDE_DIRS}
     PATH_SUFFIXES libxml++-2.6 include/libxml++-2.6
     )
@@ -24,6 +34,7 @@ find_path(LibXML++_INCLUDE_DIR
 # Glib-related libraries also use a separate config header, which is in lib dir
 find_path(LibXML++Config_INCLUDE_DIR
     NAMES libxml++config.h
+    HINTS "${LIBXMLXX_ROOT_DIR}/include"
     PATHS ${LibXML++_PKGCONF_INCLUDE_DIRS} /usr
     PATH_SUFFIXES lib/libxml++-2.6/include
     )
@@ -31,6 +42,7 @@ find_path(LibXML++Config_INCLUDE_DIR
 # find lib
 find_path(LibXML++_PKGCONF_LIBRARY_DIRS
     NAMES xml++-2.6
+    HINTS "${LIBXMLXX_ROOT_DIR}/lib"
     PATH_SUFFIXES lib
     )
 libfind_library(LibXML++ xml++ 2.6)
