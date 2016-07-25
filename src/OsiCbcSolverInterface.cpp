d6f0f3047fa54443c1183407d95a4215290329c5
Matthew Gidden <matthew.gidden@gmail.com>
2016-07-25 10:10:10 +0200

    remove 2.8 osi interface in favor of 2.9, add a warning if > 2.9

--- src/OsiCbcSolverInterface.cpp
+++ src/OsiCbcSolverInterface.cpp
@@ -3,8 +3,6 @@
 // CBC_VERSION_MAJOR defined for Cbc > 2.5
 #ifndef CBC_VERSION_MAJOR
 #include "OsiCbcSolverInterface_2_5.cpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-#include "OsiCbcSolverInterface_2_8.cpp"
 #else
 #include "OsiCbcSolverInterface_2_9.cpp"
 #endif
--- src/OsiCbcSolverInterface.hpp
+++ src/OsiCbcSolverInterface.hpp
@@ -9,9 +9,13 @@
 // CBC_VERSION_MAJOR defined for Cbc > 2.5
 #ifndef CBC_VERSION_MAJOR
 #include "OsiCbcSolverInterface_2_5.hpp"
-#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 8
-#include "OsiCbcSolverInterface_2_8.hpp"
+#elif CBC_VERSION_MAJOR == 2 && CBC_VERSION_MINOR <= 9
+#include "OsiCbcSolverInterface_2_9.hpp"
 #else
+#pragma message("WARNING: Cbc version " + CBC_VERSION_MAJOR + "." +     \
+                CBC_VERSION_MINOR + \
+                " detected. The interface may be unstable and could " + \
+                "be a possible source of segfaults.")
 #include "OsiCbcSolverInterface_2_9.hpp"
 #endif
 
